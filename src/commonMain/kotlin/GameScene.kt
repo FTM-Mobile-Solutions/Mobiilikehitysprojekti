e5b4af97ecc2534c1a5280e3985892ec6ef64c2c
LeeviJarvinen <t0jale00@students.oamk.fi>
2023-03-29 16:18:41 +0300

    Kenttä luokkaa edistetty ja luokka ladataan gamesceneen

--- src/commonMain/kotlin/GameScene.kt
+++ src/commonMain/kotlin/GameScene.kt
@@ -8,52 +8,32 @@ import com.soywiz.korio.async.*
 class GameScene : Scene() {
     private val fieldMargin = 15
 
-    private lateinit var bg: Bitmap
-    private lateinit var ground: Bitmap
-    private lateinit var platform: Bitmap
+//    private lateinit var bg: Bitmap
+//    private lateinit var ground: Bitmap
+//    private lateinit var platform: Bitmap
     private lateinit var player: Player
-    private lateinit var groundHitbox: SolidRect
-    private lateinit var platformHitbox: SolidRect
+    private lateinit var level: Level
+//    private lateinit var groundHitbox: SolidRect
+//    private lateinit var platformHitbox: SolidRect
 
-    private var gravity = 7000.0
-    private var velocityY = 0.0
+    private var gravity = 5000.0
+    private var velocityY = 100.0
 
     override suspend fun SContainer.sceneInit() {
-
-        bg = loadImage("testibg.png")
-        val bgimage = image(bg) {
-            smoothing = false
-        }
-        ground = loadImage("platformtest.png")
-        val groundimage = image(ground) {
-            smoothing = false
-            position(0, 500)
-        }
-        platform = loadImage("konaplatformtest.png")
-        val platformimage = image(platform) {
-            smoothing = false
-            position(720, 350)
-        }
-        groundHitbox = solidRect(width = ground.width, height = ground.height) {
-            alpha = 0.0
-            position(0, 500)
-        }
-        platformHitbox = solidRect(width = platform.width, height = platform.height) {
-            alpha = 0.0
-            position(720, 350)
-        }
 
         val flagimage = image(loadImage("goal.png")) {
             position(1100, 315)
             smoothing = false
         }
 
-        addChild(flagimage)
-        addChild(platformHitbox)
-        addChild(groundHitbox)
+        level = Level()
+        level.level1()
+
         player = Player()
         player.load()
         player.position(views.virtualWidth / 2, views.virtualHeight / 2)
+
+        addChild(level)
         addChild(player)
 
         //camera adjust, not working currently
@@ -98,14 +78,18 @@ class GameScene : Scene() {
         }
     }
     private fun checkCollisions(dt: TimeSpan) {
-        if (player.collidesWith(groundHitbox)) {
-            if (player.y + player.height > groundHitbox.y) {
-                player.y = groundHitbox.y - player.height
+        if (player.collidesWith(level.groundHitbox)) {
+            if (player.y + player.height > level.groundHitbox.y) {
+                player.y = level.groundHitbox.y - player.height
             }
         }
-        if (player.collidesWith(platformHitbox)) {
-            if (player.y + player.height > platformHitbox.y) {
-                player.y = platformHitbox.y - player.height
+        for (hitbox in level.platformHitboxes)
+        if (player.collidesWith(hitbox)) {
+            if (player.y + player.height > hitbox.y) {
+                player.y = hitbox.y - player.height
+            }
+            if (player.y - player.height < hitbox.y) { //Gonahtanut logiikka siihen että ei mene alustasta alhaalta läpi
+                player.y = hitbox.y + player.height * 2
             }
         }
     }
--- src/commonMain/kotlin/Level.kt
+++ src/commonMain/kotlin/Level.kt
@@ -1,3 +1,56 @@
-class Level {
-    // Leveleiden muodostaminen
+import com.soywiz.korge.view.*
+import com.soywiz.korim.bitmap.*
+
+class Level : Container() {
+    enum class State {
+        LEVEL_1,
+        LEVEL_2,
+        LEVEL_3
+    }
+
+     lateinit var bg: Bitmap
+     lateinit var ground: Bitmap
+     lateinit var platform: Bitmap
+     lateinit var groundHitbox: SolidRect
+    val platformHitboxes = mutableListOf<SolidRect>()
+//     lateinit var platformHitboxes: List<SolidRect>
+
+    lateinit var state: State
+
+    suspend fun level1() {
+        state = State.LEVEL_1
+        bg = loadImage("testibg.png")
+        val bgimage = image(bg) {
+            smoothing = false
+        }
+        ground = loadImage("platformtest.png")
+        val groundimage = image(ground) {
+            smoothing = false
+            position(0,500)
+        }
+        groundHitbox = solidRect(width = ground.width, height = ground.height) {
+            alpha = 0.0
+            position(0, 500)
+        }
+
+
+        createplatform(1000, 350)
+        createplatform(700, 350)
+        createplatform(400, 350)
+        createplatform(100, 350)
+
+    }
+
+    suspend fun createplatform(gx:Int, gy:Int) {
+        platform = loadImage("konaplatformtest.png")
+        val platformimage = image(platform) {
+            smoothing = false
+            position(gx, gy)
+        }
+        val platformHitbox = solidRect(width = platform.width, height = platform.height) {
+            alpha = 0.0
+            position(gx, gy)
+        }
+        platformHitboxes.add(platformHitbox)
+    }
 }
