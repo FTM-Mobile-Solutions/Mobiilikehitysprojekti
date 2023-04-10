38e1b149d51606ba1fd2d53dd1d058b0b575cebc
Hakis1 <t1hajo02@students.oamk.fi>
2023-04-10 15:36:06 +0300

    GameOver sceneen siirtyminen menuun tai uudelleen yrittämään

--- src/commonMain/kotlin/GameOver.kt
+++ src/commonMain/kotlin/GameOver.kt
@@ -10,6 +10,8 @@ import com.soywiz.korio.file.std.*
 class GameOver: Scene() {
     private lateinit var bg: Image
     private lateinit var go: Image
+    private lateinit var playAgainText: Text
+    private lateinit var mainMenuText: Text
     override suspend fun SContainer.sceneInit() {
 
         bg = image(resourcesVfs["tiles/bg.png"].readBitmap()) {
@@ -24,18 +26,41 @@ class GameOver: Scene() {
             smoothing = false
             alpha = 0.0
         }
-        go.onClick {
+
+        playAgainText = text("Play Again?", textSize = 24.0) {
+            centerOnStage()
+            tint = Colors.RED
+            y += 100.0
+            alpha = 0.0
+        }
+
+        mainMenuText = text("Main Menu", textSize = 24.0) {
+            centerOnStage()
+            tint = Colors.RED
+            y += 150
+            alpha = 0.0
+        }
+
+        playAgainText.onClick {
             sceneContainer.changeTo<GameScene>()
+        }
+
+        mainMenuText.onClick {
+            sceneContainer.changeTo<MainScene>()
         }
     }
 
     override suspend fun sceneAfterInit() {
         bg.tween(bg::alpha[1.0], time = 1.seconds)
         go.tween(go::alpha[1.0], go::scale[1.0], time = 1.seconds)
+        playAgainText.tween(playAgainText::alpha[1.0], time = 1.seconds)
+        mainMenuText.tween(mainMenuText::alpha[1.0], time = 1.seconds)
     }
 
     override suspend fun sceneBeforeLeaving() {
         bg.tween(bg::alpha[0.0], time = .5.seconds)
         go.tween(go::alpha[0.0], go::scale[0.0], time = .5.seconds)
+        playAgainText.tween(playAgainText::alpha[0.0], time = .5.seconds)
+        mainMenuText.tween(mainMenuText::alpha[0.0], time = .5.seconds)
     }
 }
