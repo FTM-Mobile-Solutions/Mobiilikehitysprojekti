e50017aca367cb5e008cad983ee72ac6037b8517
LeeviJarvinen <t0jale00@students.oamk.fi>
2023-04-20 17:43:16 +0300

    korjaus mainsceneen

--- src/commonMain/kotlin/MainScene.kt
+++ src/commonMain/kotlin/MainScene.kt
@@ -1,15 +1,13 @@
 import com.soywiz.klock.*
 import com.soywiz.korau.sound.*
-import com.soywiz.korge.animate.*
 import com.soywiz.korge.input.*
 import com.soywiz.korge.scene.*
 import com.soywiz.korge.tween.*
 import com.soywiz.korge.view.*
-import com.soywiz.korge.view.onClick
+import com.soywiz.korim.color.*
+import com.soywiz.korim.font.*
 import com.soywiz.korim.format.*
-import com.soywiz.korio.async.*
 import com.soywiz.korio.file.std.*
-import com.soywiz.korma.geom.*
 
 class MainScene : Scene() {
     private lateinit var bg: Image
